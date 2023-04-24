0c4b38c077650709659a8f5f953fdb72600f519e
sahinmelikov <1rima621melikov@gmail.com>
2023-04-24 23:08:56 +0400

    Reflection Task

---
+++ Reflection/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            user.GetFulLName();
+            User.ChangeAge(90);
+
+            Type type = typeof(User);
+
+            Console.WriteLine("Id=============================================");
+            PropertyInfo prop_id = type.GetProperty("_id", BindingFlags.Instance | BindingFlags.NonPublic);
+            prop_id.SetValue(user, 4);
+            Console.WriteLine(prop_id.GetValue(user));
+
+            Console.WriteLine("Name==========================================");
+            PropertyInfo prop_name = type.GetProperty("_name", BindingFlags.Instance | BindingFlags.NonPublic);
+            prop_name.SetValue(user, "Sahin");
+            Console.WriteLine(prop_name.GetValue(user));
+
+            Console.WriteLine("Surname==========================================");
+            PropertyInfo prop_surname = type.GetProperty("_surname", BindingFlags.Instance | BindingFlags.NonPublic);
+            prop_surname.SetValue(user, "Melikov");
+            Console.WriteLine(prop_surname.GetValue(user));
+
+
+
+
+
+            Console.WriteLine("Age==========================================");
+            PropertyInfo prop_age = type.GetProperty("_age", BindingFlags.Instance | BindingFlags.NonPublic|BindingFlags.Static);
+            prop_age.SetValue(null, 190);
+            Console.WriteLine(prop_age.GetValue(null));
+
+
+            MethodInfo methodget = type.GetMethod("UserInformation");
+            methodget.Invoke(user, null);
+        }
+      
+    }
+}
---
+++ Reflection/User.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    internal class User
+    {
+        public User()
+        {
+            
+        }
+        private int _id { get; set; }
+        private string _name{ get; set; } 
+        private string _surname { get; set; }
+        private static int _age { get; set; }
+  
+        public void GetFulLName()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type[] types = assembly.GetTypes();
+            foreach (Type type in types)
+            {
+                Console.WriteLine("Name===========");
+                Console.WriteLine(type.Name);
+                Console.WriteLine("FullName=========");
+                Console.WriteLine(type.FullName);
+                //var fealds = type.GetFields(BindingFlags.Instance|BindingFlags.NonPublic);
+                //foreach (var feald in fealds)
+                //{
+                //    Console.WriteLine(feald.Name);
+                //    Console.WriteLine(feald);
+
+                //}
+
+            }
+
+        }
+       public static void ChangeAge(int age)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type[] types = assembly.GetTypes();
+            foreach (var item in types)
+            {
+              _age = age;
+                Console.WriteLine(age);
+            
+            }
+
+        }
+
+        public void UserInformation()
+        {
+            Console.WriteLine(_id+" "+_name+" "+_surname+" "+_age);
+        }
+
+    }
+ 
+
+
+}
+    
+
